94b9f8f449431f82830ce49c5957d2c4411110e1
Alexandre-HCS <holy.cow3728@gmail.com>
2024-11-26 16:22:58 -0300

    V2

--- conexao.php
+++ conexao.php
@@ -14,8 +14,8 @@ if ($conn->connect_error) {
 $sql = "SHOW COLUMNS FROM produtos LIKE 'imagem'";
 $result = $conn->query($sql);
 if ($result->num_rows == 0) {
-$sql = "ALTER TABLE produtos ADD COLUMN imagem VARCHAR(255)";
-$conn->query($sql);
+    $sql = "ALTER TABLE produtos ADD COLUMN imagem VARCHAR(255)";
+    $conn->query($sql);
 }
 // Adiciona a coluna 'imagem' à tabela 'fornecedores' se ela não existir
 $sql = "SHOW COLUMNS FROM fornecedores LIKE 'imagem' ";
